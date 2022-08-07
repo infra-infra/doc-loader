8741e1e627687191352207518f8d07c818f92713
cc <44751363+zzfn@users.noreply.github.com>
2022-08-07 15:47:24 +0800

    feat: a

--- packages/site-components/components/index.ts
+++ packages/site-components/components/index.ts
@@ -2,8 +2,6 @@ export { default as CellCode } from './CellCode';
 
 export { default as CellDemo } from './CellDemo';
 
-export { default as Changelog } from './Changelog';
-
 export { default as CellDescription } from './CellDescription';
 
 export { default as CodeBlockWrapper } from './CodeBlockWrapper';
---
+++ packages/site-components/components/style/index.ts
@@ -0,0 +1,2 @@
+import './index.less'
+import './highlight.less'
